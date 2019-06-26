c491dd3470e0a0bf8ebe645d1fdc66f53c5173c1
Sean Parker <sean.parker067@gmail.com>
2019-06-26 19:56:24 +0100

    Remove old bg, add new bg, add gitignore

--- static/js/timer.js
+++ static/js/timer.js
@@ -8,7 +8,8 @@ updateTime();
 
 function updateTime() {
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = now - now;
+  //var distance = countDownDate - now;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
